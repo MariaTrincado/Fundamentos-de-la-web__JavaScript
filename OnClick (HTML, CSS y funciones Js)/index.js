434edbad39ed0d89f205c8106179d7d06d4daf5b
María Trincado <maria_trincado@hotmail.com>
2023-05-25 15:08:18 -0400

    no funciona link

--- OnClick (HTML, CSS y funciones Js)/index.js
+++
@@ -1,9 +0,0 @@
-function eliminarContenido(boton) {
-    boton.style.display = "none";
-}
-function cambiarLogin() {
-    document.getElementById("btn1").innerHTML = "Logout"; 
-}
-function mostrarAlerta(){
-    alert("Ninja was liked");
-}
