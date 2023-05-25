594640ed4e7d663a0a5888bcc4644a9b00e235af
María Trincado <maria_trincado@hotmail.com>
2023-05-25 15:02:14 -0400

    Jue25_Sem2

---
+++ OnClick (HTML, CSS y funciones Js)/index.js
@@ -0,0 +1,9 @@
+function eliminarContenido(boton) {
+    boton.style.display = "none";
+}
+function cambiarLogin() {
+    document.getElementById("btn1").innerHTML = "Logout"; 
+}
+function mostrarAlerta(){
+    alert("Ninja was liked");
+}
