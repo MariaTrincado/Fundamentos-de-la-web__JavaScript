2aa026f16bb87410a964cd25dfb616f5d11d074a
María Trincado <maria_trincado@hotmail.com>
2023-05-29 03:17:08 -0400

    Vi26_sem2

---
+++ Me gusta/index.js
@@ -0,0 +1,18 @@
+function aumentarLike1() {
+    var spanElement = document.getElementById('b1');
+    var conteo = parseInt(spanElement.innerText); 
+    conteo += 1; 
+    spanElement.innerText = conteo; 
+}
+function aumentarLike2() {
+    var spanElement = document.getElementById('b2');
+    var conteo = parseInt(spanElement.innerText); 
+    conteo += 1; 
+    spanElement.innerText = conteo; 
+}
+function aumentarLike3() {
+    var spanElement = document.getElementById('b3');
+    var conteo = parseInt(spanElement.innerText); 
+    conteo += 1; 
+    spanElement.innerText = conteo; 
+}
